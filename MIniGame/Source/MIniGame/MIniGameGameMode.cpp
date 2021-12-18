9cc02d0c86a3b2db4f9906fdf070ecef5b8eb9e1
Jay_Auditore <993574833@qq.com>
2021-12-19 05:34:53 +0800

    Init: Create a new project

---
+++ MIniGame/Source/MIniGame/MIniGameGameMode.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MIniGameGameMode.h"
+#include "MIniGameCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMIniGameGameMode::AMIniGameGameMode()
+{
+}
---
+++ MIniGame/Source/MIniGame/MIniGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MIniGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMIniGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMIniGameGameMode();
+};
+
+
+
